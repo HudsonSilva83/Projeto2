792a29aa03c2fd37dd96bfd5e507a8756d62681a
HudsonSilva83 <hudson@Adriele>
2017-04-23 11:21:45 -0300

    Tentando achar o botão pregoes e Consulta a pregões

--- src/test/java/Step_Definitions/Pesquisar.java
+++ src/test/java/Step_Definitions/Pesquisar.java
@@ -1,15 +1,28 @@
 package Step_Definitions;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
 
 public class Pesquisar extends BaseDriver {
-
-	CarregarPagina carrega = new CarregarPagina();
-
+WebDriverWait wait = new WebDriverWait(driver, 10);
+	
 	@Dado("^O sistema apresenta os campos para preenchimento$")
 	public void o_sistema_apresenta_os_campos_para_preenchimento() {
+		IniciarNavegador();
+		
+		Actions actions = new Actions(driver);
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("a[href*='pregao']")));
+		driver.findElement(By.xpath(".//*[@id='mPrincipal']/div/ul/li[2]"));
+		WebElement BtPregao = driver.findElement(By.cssSelector("a[href*='pregao']"));
+		actions.moveToElement(BtPregao);
 
 	}
 
