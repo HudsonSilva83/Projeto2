3533562168e33b32196e2c7190e65775af797199
HudsonSilva83 <hudson@Adriele>
2017-04-26 19:12:34 -0300

    Atualizando código

---
+++ src/test/java/Step_Definitions/VerificarHome.java
@@ -0,0 +1,54 @@
+package Step_Definitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+
+public class VerificarHome extends BaseDriver {
+
+	@Dado("^A pagina inicial carregada$")
+	public void a_pagina_inicial_carregada() {
+
+		IniciarNavegador();
+
+	}
+
+	@Dado("^Ao clicar em outra funcionalidade$")
+	public void ao_clicar_em_outra_funcionalidade() {
+
+		Actions actions = new Actions(driver);
+
+		WebElement BtPregao = driver.findElement(By.xpath(".//*[@id='mPrincipal']/div/ul/li[2]"));
+		actions.moveToElement(BtPregao).build().perform();
+
+		WebElement BtConsulta = driver.findElement(By.xpath(".//*[@id='mPrincipal']/div/ul/li[2]/ul/li[1]/a/span"));
+		actions.moveToElement(BtConsulta).click().build().perform();
+
+	}
+
+	@Dado("^clicar na pagina home$")
+	public void clicar_na_pagina_home() {
+		
+		WebDriverWait espere = new WebDriverWait(driver, 20);
+		
+		espere.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(".//*[@id='idPagInicial']")));
+		driver.findElement(By.xpath(".//*[@id='idPagInicial']")).click();
+
+	}
+
+	@Entao("^deve exibir a pagina Home novamente$")
+	public void deve_exibir_a_pagina_Home_novamente() {
+		
+		
+		WebDriverWait espere = new WebDriverWait(driver, 10);
+		
+		espere.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='corpo']/div/div[1]/p[2]")));
+	
+	}
+
+}
---
+++ src/test/resources/IdentificarHome.feature
@@ -0,0 +1,9 @@
+#language:pt;
+
+Funcionalidade: Pagina Home 
+
+Cenario: Identificar a pagina Inicial
+Dado A pagina inicial carregada 
+E Ao clicar em outra funcionalidade
+E clicar na pagina home
+Entao deve exibir a pagina Home novamente
